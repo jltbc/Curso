c02bb7d954cc6edb53cc6c7862d616dbbca5031c
julio-gabryell <juliogabryel1337@gmail.com>
2021-01-21 00:42:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const rca = 20;
+const bnf = 500;
+console.log(rca >=100);
+if((rca >= 100)|| rca <= 50 ){
+    console.log(`sua pontuação de ${rca} é maior ou menor que 100, receba ${bnf} em bonificação`)
+}
+else{
+    console.log(`você não atingiu os valores necessarios`)
+}
